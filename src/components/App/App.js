f93c5d63926aafc5e2850d06da56d101ffdb1c83
Lorcan Mc <lorcan.jmc@gmail.com>
2022-04-21 13:59:08 +0100

    added bootstrap header

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { NavBarContainer } from '../NavBarContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarContainer />
+
+      <h1>hello there</h1>
+    </div>
+  );
+}
+
+export default App;
