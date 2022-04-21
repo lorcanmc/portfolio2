793086604033af3b31922c75b4695eb0773ee10c
Lorcan Mc <lorcan.jmc@gmail.com>
2022-04-21 14:16:46 +0100

    added singing image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { NavBarContainer } from '../NavBarContainer';
+import logo from "./logo.svg";
+import "./App.css";
+import { NavBarContainer } from "../NavBarContainer";
+import singingPic from "../../MeSinging.jfif";
 
 function App() {
   return (
     <div className="App">
       <NavBarContainer />
-
-      <h1>hello there</h1>
+      <div className="singing-pic-container">
+        <img style={{ width: "100%" }} src={singingPic} />
+        <div class="top-right">
+          <p>Lorcan McCullagh</p>
+          <p>Software Developer</p>
+        </div>
+      </div>
     </div>
   );
 }
